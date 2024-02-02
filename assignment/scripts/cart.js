a8cb521b21a0040f77ed8f270ae9c0d9ddfb4d35
Zoe Lindman <zoe.lindman@gmail.com>
2024-02-01 20:56:18 -0600

    completed assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,43 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    return true;
+    console.log(`Basket is ${basket}`);
+}
+
+addItem('item');
+
+function listItems() {
+    for(let item of basket){
+        console.log(item);
+    }
+}
+
+listItems();
+
+function empty() {
+    basket.length = 0;
+    console.log(listItems);
+}
+
+empty();
+
+var maxItems = 5;
+
+function isFull() {
+    if (basket.length < maxItems) {
+        return false;
+    }
+    else if (basket.length >= maxItems) {
+        return true;
+    }
+}
+
+isFull();
 
 
 
